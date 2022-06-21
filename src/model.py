f95432b45fb40e36761781797c6466b34276f54c
kimjh0107 <kimjh971717@gmail.com>
2022-06-21 10:09:59 +0900

    resnet layer change

--- src/model.py
+++ src/model.py
@@ -10,20 +10,21 @@ from src.device import get_device
 class CNN(nn.Module):
     def __init__(self, in_channels = 1, num_classes = 2):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv3d(in_channels=1, out_channels=32, kernel_size=3, stride = 1, padding = 1)
-        self.conv2 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, stride = 1, padding = 1)
-        self.conv3 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride = 1, padding = 1)
-        self.conv4 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=3, stride = 1, padding = 1)
-        self.conv5 = nn.Conv3d(in_channels=128, out_channels=256, kernel_size=3, stride = 1, padding = 1)
-        self.conv6 = nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3, stride = 1, padding = 1)
+        self.conv1 = nn.Conv3d(in_channels=1, out_channels=32, kernel_size=3, stride = 1)
+        self.conv2 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, stride = 1)
+        self.conv3 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride = 1)
+        self.conv4 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=3, stride = 1)
+        self.conv5 = nn.Conv3d(in_channels=128, out_channels=256, kernel_size=3, stride = 1)
+        self.conv6 = nn.Conv3d(in_channels=256, out_channels=512, kernel_size=3, stride = 1)
 
        # self.conv5 = nn.Conv3d(in_channels=128, out_channels=128, kernel_size=3, stride = 1, padding=1)
         self.pool = nn.MaxPool3d(kernel_size=(2,2,2))
         self.dropout = nn.Dropout(0.2)
-        self.fc = nn.Linear(2048, num_classes)
+        self.fc = nn.Linear(150528, num_classes)
         self.BatchNorm1 = nn.BatchNorm3d(32)
         self.BatchNorm2 = nn.BatchNorm3d(64)
-        self.BatchNorm3 = nn.BatchNorm3d(512)
+        self.BatchNorm3 = nn.BatchNorm3d(128)
+        self.BatchNorm4 = nn.BatchNorm3d(512)
 
     def forward(self, x):
         x = x.view([x.size(0), 1, 64, 128, 128])
@@ -31,28 +32,75 @@ class CNN(nn.Module):
 
         x = F.relu(self.conv1(x))
         x = self.pool(x)
+        x = self.BatchNorm1(x) 
 
         x = F.relu(self.conv2(x))
         x = self.pool(x)
-
-        x = F.relu(self.conv3(x))
-        x = self.pool(x)
+        x = self.BatchNorm2(x) 
+        # x = F.relu(self.conv3(x))
+        # x = self.pool(x)
 
         x = F.relu(self.conv4(x))
         x = self.pool(x)    
-
-        x = F.relu(self.conv5(x))
-        x = self.pool(x)  
-
-        x = F.relu(self.conv6(x))
-        x = self.pool(x)  
         x = self.BatchNorm3(x) 
+
+        # x = F.relu(self.conv5(x))
+        # x = self.pool(x)  
+
+        # x = F.relu(self.conv6(x))
         x = self.dropout(x)
 
         x = x.reshape(x.shape[0], -1) 
         x = self.fc(x)
 
         return x 
+
+
+# class CNN(nn.Module):
+#     def __init__(self, in_channels = 1, num_classes = 2):
+#         super(CNN, self).__init__()
+#         self.conv1 = nn.Conv3d(in_channels=1, out_channels=12, kernel_size=3, stride = 2)
+#         self.conv2 = nn.Conv3d(in_channels=12, out_channels=24, kernel_size=3, stride = 2)
+#         self.conv3 = nn.Conv3d(in_channels=24, out_channels=64, kernel_size=3, stride = 2)
+#         self.conv4 = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=3, stride = 2)
+
+#         self.pool = nn.MaxPool3d(kernel_size=(2,2,2))
+#         self.dropout = nn.Dropout(0.2)
+#         self.fc = nn.Linear(262144, num_classes)
+#         self.BatchNorm1 = nn.BatchNorm3d(32)
+#         self.BatchNorm2 = nn.BatchNorm3d(64)
+#         self.BatchNorm3 = nn.BatchNorm3d(128)
+#         self.BatchNorm4 = nn.BatchNorm3d(512)
+
+#     def forward(self, x):
+#         x = x.view([x.size(0), 1, 64, 128, 128])
+#      #   print(x.shape)  
+
+#         x = F.relu(self.conv1(x))
+#         x = self.pool(x)
+#         x = self.BatchNorm1(x) 
+
+#         x = F.relu(self.conv2(x))
+#         x = self.pool(x)
+#         x = self.BatchNorm2(x) 
+#         # x = F.relu(self.conv3(x))
+#         # x = self.pool(x)
+
+#         x = F.relu(self.conv4(x))
+#         x = self.pool(x)    
+#         x = self.BatchNorm3(x) 
+
+#         # x = F.relu(self.conv5(x))
+#         # x = self.pool(x)  
+
+#         # x = F.relu(self.conv6(x))
+#         x = self.dropout(x)
+
+#         x = x.reshape(x.shape[0], -1) 
+#         x = self.fc(x)
+
+#         return x 
+
 
 def create_model(device):
     return CNN(num_classes=2).to(device)
@@ -149,13 +197,13 @@ class ResNet(nn.Module):  # [3,4,6,3]-> list , tell us how many times we want to
             padding=1)
 
         # ResNet layers 
-        self.layer1 = self._make_layer(block, layers[0], intermediate_channels=64, stride=1)
-        self.layer2 = self._make_layer(block, layers[1], intermediate_channels=128,stride=2)
-        self.layer3 = self._make_layer(block, layers[2], intermediate_channels=256, stride=2)
-        self.layer4 = self._make_layer(block, layers[3], intermediate_channels=512, stride=2)
+        self.layer1 = self._make_layer(block, layers[0], intermediate_channels=32, stride=1)
+        self.layer2 = self._make_layer(block, layers[1], intermediate_channels=64,stride=2)
+        self.layer3 = self._make_layer(block, layers[2], intermediate_channels=64, stride=2)
+        self.layer4 = self._make_layer(block, layers[3], intermediate_channels=128, stride=2)
 
         self.avgpool = nn.AdaptiveAvgPool3d((1,1,1))
-        self.fc = nn.Linear(512*4, num_classes)
+        self.fc = nn.Linear(128*4, num_classes)
      #   self.dropout = nn.Dropout(0.5)
      #   self.BatchNorm3 = nn.BatchNorm3d(512)
 
@@ -218,6 +266,4 @@ def ResNet152(img_channel = 1, num_classes = 1000):
     return ResNet(block, [3,8, 36,3], img_channel, num_classes)
 
 def test(device):
-    return ResNet50(1,2).to(device)
-
-
+    return ResNet152(1,2).to(device)
